f14dfdadd08b4c5e2e87eeb748176381163ca79d
sion567 <461959180@qq.com>
2026-01-26 19:08:49 +0900

    添加Actuator

--- server/src/main/java/com/example/demo/config/SecurityConfiguration.java
+++ server/src/main/java/com/example/demo/config/SecurityConfiguration.java
@@ -43,7 +43,8 @@ public class SecurityConfiguration {
             "/configuration/security",
             "/swagger-ui/**",
             "/webjars/**",
-            "/swagger-ui.html"};
+            "/swagger-ui.html",
+            "/actuator/**"};
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
     private final LogoutHandler logoutHandler;
